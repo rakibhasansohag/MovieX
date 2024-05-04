6311448a3c5744efd793e8864ed9bb7df76fdf16
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2024-05-05 02:29:00 +0600

    switching tabs componente added

---
+++ Features.md
@@ -0,0 +1,1 @@
+<!-- TODO: there will be a crazy feature if the user click any where in the webpage there will a stan that will animate to right side of the webpage just like Switching Tabs component work -->
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
-import HeroBanner from "./heroBanner/HeroBanner";
 import "./style.scss";
+import HeroBanner from "./heroBanner/HeroBanner";
+import Trending from "./trending/Trending";
 
 function Home() {
   return (
     <div className="homePage">
       <HeroBanner />
+      <Trending />
     </div>
   );
 }
