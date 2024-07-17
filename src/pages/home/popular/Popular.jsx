bdb586227a4d1239476045b4054ee2ea63246b6c
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2024-07-18 00:43:04 +0600

    bug fixed for undefined url paremater

--- src/pages/home/popular/Popular.jsx
+++ src/pages/home/popular/Popular.jsx
@@ -1,31 +1,31 @@
-import { useState } from "react";
+import { useState } from 'react';
 
-import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
-import SwitchTabs from "../../../components/switchTabs/SwitchTabs";
-import useFetch from "../../../hooks/useFetch";
-import Carousel from "../../../components/carousel/Carousel";
+import ContentWrapper from '../../../components/contentWrapper/ContentWrapper';
+import SwitchTabs from '../../../components/switchTabs/SwitchTabs';
+import useFetch from '../../../hooks/useFetch';
+import Carousel from '../../../components/carousel/Carousel';
 useFetch;
 
 const Popular = () => {
-  const [endPoint, setEndPoint] = useState("movie");
+	const [endPoint, setEndPoint] = useState('movie');
 
-  const { data, loading } = useFetch(`/${endPoint}/popular`);
+	const { data, loading } = useFetch(`/${endPoint}/popular`);
 
-  const onTabChange = (tab) => {
-    // console.log(tab);
-    setEndPoint(tab === "Movies" ? "movie" : "tv");
-  };
+	const onTabChange = (tab) => {
+		// console.log(tab);
+		setEndPoint(tab === 'Movies' ? 'movie' : 'tv');
+	};
 
-  return (
-    <div className="carouselSection">
-      <ContentWrapper>
-        <span className="carouselTitle">What&#39;s Popular</span>
-        <SwitchTabs data={["Movies", "TV Shows"]} onTabChange={onTabChange} />
-      </ContentWrapper>
+	return (
+		<div className='carouselSection'>
+			<ContentWrapper>
+				<span className='carouselTitle'>What&#39;s Popular</span>
+				<SwitchTabs data={['Movies', 'TV Shows']} onTabChange={onTabChange} />
+			</ContentWrapper>
 
-      <Carousel data={data?.results} loading={loading} endPoint={endPoint} />
-    </div>
-  );
+			<Carousel data={data?.results} loading={loading} endpoint={endPoint} />
+		</div>
+	);
 };
 
 export default Popular;
--- src/pages/home/topRated/TopRated.jsx
+++ src/pages/home/topRated/TopRated.jsx
@@ -1,32 +1,32 @@
-import { useState } from "react";
+import { useState } from 'react';
 
-import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
-import SwitchTabs from "../../../components/switchTabs/SwitchTabs";
-import useFetch from "../../../hooks/useFetch";
-import Carousel from "../../../components/carousel/Carousel";
+import ContentWrapper from '../../../components/contentWrapper/ContentWrapper';
+import SwitchTabs from '../../../components/switchTabs/SwitchTabs';
+import useFetch from '../../../hooks/useFetch';
+import Carousel from '../../../components/carousel/Carousel';
 useFetch;
 
 const TopRated = () => {
-  const [endPoint, setEndPoint] = useState("movie");
+	const [endPoint, setEndPoint] = useState('movie');
 
-  // Reference:  https://developer.themoviedb.org/reference/movie-top-rated-list
-  const { data, loading } = useFetch(`/${endPoint}/top_rated`);
+	// Reference:  https://developer.themoviedb.org/reference/movie-top-rated-list
+	const { data, loading } = useFetch(`/${endPoint}/top_rated`);
 
-  const onTabChange = (tab) => {
-    // console.log(tab);
-    setEndPoint(tab === "Movies" ? "movie" : "tv");
-  };
+	const onTabChange = (tab) => {
+		// console.log(tab);
+		setEndPoint(tab === 'Movies' ? 'movie' : 'tv');
+	};
 
-  return (
-    <div className="carouselSection">
-      <ContentWrapper>
-        <span className="carouselTitle">Top Rated</span>
-        <SwitchTabs data={["Movies", "TV Shows"]} onTabChange={onTabChange} />
-      </ContentWrapper>
+	return (
+		<div className='carouselSection'>
+			<ContentWrapper>
+				<span className='carouselTitle'>Top Rated</span>
+				<SwitchTabs data={['Movies', 'TV Shows']} onTabChange={onTabChange} />
+			</ContentWrapper>
 
-      <Carousel data={data?.results} loading={loading} endPoint={endPoint} />
-    </div>
-  );
+			<Carousel data={data?.results} loading={loading} endpoint={endPoint} />
+		</div>
+	);
 };
 
 export default TopRated;
