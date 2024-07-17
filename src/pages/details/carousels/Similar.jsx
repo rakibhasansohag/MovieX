3dc8e9e5e76c1aaaba5da2faf0652756cc581141
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2024-07-18 00:48:30 +0600

    props type added

--- src/pages/details/carousels/Similar.jsx
+++ src/pages/details/carousels/Similar.jsx
@@ -1,19 +1,26 @@
-import Carousel from "../../../components/carousel/Carousel";
-import useFetch from "../../../hooks/useFetch";
+import PropTypes from 'prop-types';
+
+import Carousel from '../../../components/carousel/Carousel';
+import useFetch from '../../../hooks/useFetch';
 
 const Similar = ({ mediaType, id }) => {
-  const { data, loading, error } = useFetch(`/${mediaType}/${id}/similar`);
+	const { data, loading } = useFetch(`/${mediaType}/${id}/similar`);
 
-  const title = mediaType === "tv" ? "Similar TV Shows" : "Similar Movies";
+	const title = mediaType === 'tv' ? 'Similar TV Shows' : 'Similar Movies';
 
-  return (
-    <Carousel
-      title={title}
-      data={data?.results}
-      loading={loading}
-      endpoint={mediaType}
-    />
-  );
+	return (
+		<Carousel
+			title={title}
+			data={data?.results}
+			loading={loading}
+			endpoint={mediaType}
+		/>
+	);
+};
+
+Similar.propTypes = {
+	mediaType: PropTypes.oneOf(['movie', 'tv']).isRequired,
+	id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
 };
 
 export default Similar;
